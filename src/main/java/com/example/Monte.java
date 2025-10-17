e62e5fe602a1ea3e8ec9649199ca8c0cdae66e2f
ryan.asilva14 <ryansoftdev@gmail.com>
2025-10-17 19:59:53 -0300

    Criação da classe Computador e Monte.

---
+++ src/main/java/com/example/Monte.java
@@ -0,0 +1,25 @@
+package com.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Monte {
+
+    private List<Carta> carta = new ArrayList<>();
+
+    public Monte (){
+
+    }
+
+    public void embaralhar  {
+
+    }
+
+    public virar (Carta carta) {
+
+        return Carta.ge;
+
+    }
+
+
+}
